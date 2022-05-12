f3177f53ec10a236f0bc0395473132f7d8316d29
HmaniAhmed <ahmed.hmani@esprit.tn>
2022-05-12 23:22:11 +0100

    Loggers added for Time_Sheet

--- src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +23,7 @@ import tn.esprit.spring.repository.TimesheetRepository;
 
 @Service
 public class TimesheetServiceImpl implements ITimesheetService {
-	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TimesheetServiceImpl.class);
 
 	@Autowired
 	MissionRepository missionRepository;
@@ -38,38 +40,48 @@ public class TimesheetServiceImpl implements ITimesheetService {
 	}
     
 	public void affecterMissionADepartement(int missionId, int depId) {
+		LOGGER.info(" affecter mission a de partement en exécution ");
+		LOGGER.debug("commencer a chercher id mission");
 		Mission mission = missionRepository.findById(missionId).get();
+		LOGGER.debug("commencer a chercher id departement");
 		Departement dep = deptRepoistory.findById(depId).get();
+		LOGGER.debug("update departement");
 		mission.setDepartement(dep);
 		missionRepository.save(mission);
+		LOGGER.debug("fin");
 		
 	}
 
 	public void ajouterTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin) {
+		LOGGER.info(" Debut ajouterTimesheet ");
 		TimesheetPK timesheetPK = new TimesheetPK();
 		timesheetPK.setDateDebut(dateDebut);
 		timesheetPK.setDateFin(dateFin);
 		timesheetPK.setIdEmploye(employeId);
 		timesheetPK.setIdMission(missionId);
+		LOGGER.debug("champ updated");
 		
 		Timesheet timesheet = new Timesheet();
 		timesheet.setTimesheetPK(timesheetPK);
-		timesheet.setValide(false); //par defaut non valide
+		timesheet.setValide(false); 
 		timesheetRepository.save(timesheet);
+		LOGGER.info(" timesheet ajouter avec succées");
 		
 	}
 
 	
 	public void validerTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin, int validateurId) {
-		System.out.println("In valider Timesheet");
+		LOGGER.info(" In valider Timesheet ");
+		LOGGER.debug("commencer a chercher id employe validateur");
 		Employe validateur = employeRepository.findById(validateurId).get();
+		LOGGER.debug("commencer a chercher id mission");
 		Mission mission = missionRepository.findById(missionId).get();
-		//verifier s'il est un chef de departement (interet des enum)
+		LOGGER.debug("verifier s'il est un chef de departement");
 		if(!validateur.getRole().equals(Role.CHEF_DEPARTEMENT)){
 			System.out.println("l'employe doit etre chef de departement pour valider une feuille de temps !");
 			return;
 		}
-		//verifier s'il est le chef de departement de la mission en question
+		LOGGER.debug("verifier s'il est le chef de departement de la mission en question");
 		boolean chefDeLaMission = false;
 		for(Departement dep : validateur.getDepartements()){
 			if(dep.getId() == mission.getDepartement().getId()){
@@ -78,17 +90,22 @@ public class TimesheetServiceImpl implements ITimesheetService {
 			}
 		}
 		if(!chefDeLaMission){
+			LOGGER.info("l'employe n'est pas un chef de la mission");
 			System.out.println("l'employe doit etre chef de departement de la mission en question");
+			LOGGER.error("l'employer n'est pas un chef départtement");
 			return;
+			
 		}
-//
+		LOGGER.debug("l'ajout de  timesheet");
 		TimesheetPK timesheetPK = new TimesheetPK(missionId, employeId, dateDebut, dateFin);
 		Timesheet timesheet =timesheetRepository.findBytimesheetPK(timesheetPK);
 		timesheet.setValide(true);
+		LOGGER.debug("update timesheet");
 		
-		//Comment Lire une date de la base de données
+	
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		System.out.println("dateDebut : " + dateFormat.format(timesheet.getTimesheetPK().getDateDebut()));
+		LOGGER.info("valider timesheet avec succées ");
 		
 	}
 
