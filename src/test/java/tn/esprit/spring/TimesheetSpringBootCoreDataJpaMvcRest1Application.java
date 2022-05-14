5aabe83d44eb5f6dbb33011131c1a940761d3c54
heniAbdmouleh <heni.abdmouleh1@esprit.tn>
2022-05-14 02:33:56 +0200

    update TimesheetSpringBootCoreDataJpaMvcRest1Application

--- src/test/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
+++ src/test/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
@@ -1,14 +1,10 @@
-
 package tn.esprit.spring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-public class TimesheetSpringBootCoreDataJpaMvcRest1Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TimesheetSpringBootCoreDataJpaMvcRest1Application.class, args);
-	}
-
+@SpringBootTest
+class TimesheetSpringBootCoreDataJpaMvcRestApplicationTests {
+	
+	
 }
